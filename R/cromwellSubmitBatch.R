b8b29678dbafd050c4cd23e0713e0629aa0511e9
Amy Paguirigan <amypaguirigan@gmail.com>
2020-04-10 16:16:29 -0700

    batch second

--- R/cromwellSubmitBatch.R
+++ R/cromwellSubmitBatch.R
@@ -26,9 +26,8 @@ cromwellSubmitBatch <-
     }
     bodyList <- list(
       workflowSource = httr::upload_file(WDL))
-
-    if(is.null(Batch) == F) bodyList <- c(bodyList, workflowInputs = list(httr::upload_file(Batch)))
-    if(is.null(Params) == F) bodyList <- c(bodyList, workflowInputs_2 = list(httr::upload_file(Params)))
+    if(is.null(Params) == F) bodyList <- c(bodyList, workflowInputs = list(httr::upload_file(Params)))
+    if(is.null(Batch) == F) bodyList <- c(bodyList, workflowInputs_2 = list(httr::upload_file(Batch)))
     if(is.null(Dependencies) == F) bodyList <- c(bodyList, workflowDependencies = list(httr::upload_file(Dependencies)))
     if(is.null(Options) == F) bodyList <- c(bodyList, workflowOptions = list(httr::upload_file(Options)))
     if(is.null(Labels) == F) bodyList <- c(bodyList, labels = list(jsonlite::toJSON(as.list(Labels), auto_unbox = TRUE)))
