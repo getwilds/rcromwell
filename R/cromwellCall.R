aba1d92d72f9667ff43c3d867dcb6c4154343d62
Amy Paguirigan <amypaguirigan@gmail.com>
2021-02-11 20:54:35 -0800

    Update cromwell Call for subworkflows

--- R/cromwellCall.R
+++ R/cromwellCall.R
@@ -9,6 +9,7 @@
 #' @details
 #' Requires valid Cromwell server URL to be set in the environment, or the use
 #' of the cromURL param if you want to specify upon call the URL to use. (use `setCromwellURL()`)
+#' Now supports nested scatters.
 #' @examples
 #' ## Request what jobs have been submitted to your Cromwell instance in the past 7 days.
 #' recentJobs <- cromwellJobs(days = 7)
@@ -28,7 +29,7 @@ cromwellCall <- function(workflow_id, cromURL = Sys.getenv("CROMWELLURL", unset 
         cromURL,
         "/api/workflows/v1/",
         workflow_id,
-        "/metadata?expandSubWorkflows=false"
+        "/metadata?expandSubWorkflows=true"
       )
     ), as = "parsed")
   # if the response is a character vector, then return it and stop
@@ -37,8 +38,83 @@ cromwellCall <- function(workflow_id, cromURL = Sys.getenv("CROMWELLURL", unset 
   if (is.list(crommetadata$calls) == T) {
     # if the workflow has made calls, nab them
     bob <- purrr::pluck(crommetadata, "calls")
+    # If there is a scatter subworkflow, then take that data out and crunch it separately.
+    if (sum(grepl("ScatterAt", names(bob))) > 0 ) {
+      subs <- names(bob)[grepl("ScatterAt", names(bob))==T]
+      subworkflow <- bob[subs]
+      bob <- bob[!names(bob) %in% subs]
+      subworkflowMeta <- purrr::map(subs, function(x) {
+        b <- purrr::flatten(purrr::flatten(subworkflow[x]))
+        c <- purrr::pluck(b, "subWorkflowMetadata")
+        return(c)})
+
+      names(subworkflowMeta) <- subs
+      justSubCalls <- purrr::map(subworkflowMeta, function(subcallData) {
+        calls <- purrr::pluck(subcallData, "calls")
+        names(calls)
+        out <- purrr::map_dfr(calls, function(taskData) {
+          purrr::map_dfr(taskData, function(shards){
+            # only keep data that isn't a list itself!
+            y <- purrr::discard(shards, is.list)
+            Z <- data.frame(rbind(unlist(y)), stringsAsFactors = F)
+            # if the subworkflow failed, also return the failure metadata that is available
+            if (shards$executionStatus == "Failed") {
+              failureData <-
+                unlist(purrr::pluck(
+                  purrr::pluck(shards, "failures", .default = NA),
+                  "causedBy", "message", .default = NA
+                ))
+              if (is.na(failureData) == F) {
+                Zf <- data.frame(failureData[failureData != ""], stringsAsFactors = F)
+                Z <- cbind(Z, Zf)
+              }
+            }
+            if (is.null(shards$runtimeAttributes) == F) {
+              # if there are runtimeAttributes then.. pull them out
+              runTime <- purrr::pluck(shards, "runtimeAttributes")
+              Z1 <-data.frame(rbind(unlist(runTime)), stringsAsFactors = F)
+              Z <- cbind(Z, Z1)
+            }
+            return(Z)
+          })
+        }, .id = "fullName")
+        return(out) })  %>% purrr::map_dfr(., function(x) { x }, .id = "subWorkflowName")
+      # split fullname into workflowName and callName
+      justSubCalls <- tidyr::separate(data = justSubCalls,
+                                      col = fullName,
+                                      into = c("workflowName", "callName"),
+                                      sep = "\\.",
+                                      extra = "merge")
+
+      justSubCalls$workflow_id <- workflow_id # This lets you take this output in a map_dfr and just do rbind as the function. ;)
+      ## Big Chunk of dealing with start and end times.
+      if ("start" %in% colnames(justSubCalls) == T) {
+        # if the workflow has started
+        justSubCalls$start <- lubridate::with_tz(lubridate::ymd_hms(justSubCalls$start), tzone = "US/Pacific")
+        if ("end" %in% colnames(justSubCalls) == T) {
+          # and if end is present
+          justSubCalls$end <-lubridate::with_tz(lubridate::ymd_hms(justSubCalls$end), tzone = "US/Pacific")
+          justSubCalls <- justSubCalls %>% mutate(callDuration = ifelse(is.na(justSubCalls$end) == T,
+                                                                        round(difftime(lubridate::now(tz = "US/Pacific"), justSubCalls$start, units = "mins"),3),
+                                                                        round(difftime(justSubCalls$end, justSubCalls$start, units = "mins"), 3)))
+        } else {
+          # if end doesn't exist or it is already NA (???), make it and workflowDuration but set to NA
+          justSubCalls$end <- NA
+          justSubCalls$callDuration <- round(difftime(lubridate::now(tz = "US/Pacific"), justSubCalls$start, units = "mins"),3)
+        }
+      } else {
+        # if start doesn't exist, then create it and set it to NA
+        justSubCalls$start <- NA
+        justSubCalls$end <- NA
+        justSubCalls$callDuration <- 0
+      }
+      justSubCalls <- dplyr::mutate_all(justSubCalls, as.character)
+      justSubCalls$callDuration <- as.numeric(justSubCalls$callDuration)
+      justSubCalls$commandLine <- NULL # usually we don't need the command line
+    }
+
     if (length(bob) > 0) {
-      # this is redudant but a better error catch isn't quite clear yet.
+      # this is redundant but a better error catch isn't quite clear yet.
       justCalls <- purrr::map(bob, function(callData) {
         purrr::map_dfr(callData, function(shardData) {
           # only keep data that isn't a list itself!
@@ -99,6 +175,9 @@ cromwellCall <- function(workflow_id, cromURL = Sys.getenv("CROMWELLURL", unset 
       justCalls <- dplyr::mutate_all(justCalls, as.character)
       justCalls$callDuration <- as.numeric(justCalls$callDuration)
 
+    if (exists("justSubCalls")) {
+      justCalls <- dplyr::full_join(justCalls, justSubCalls)
+    }
     } else {
       # returns a data frame if no data is avaialable so that this can be used with Shiny apps easier
       justCalls <- data.frame("workflow_id" = "No call metadata available.", stringsAsFactors = F)
