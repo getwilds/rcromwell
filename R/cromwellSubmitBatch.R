1dbeda9afc793dd62275548c7c3ceed62e5560b6
Amy Paguirigan <amypaguirigan@gmail.com>
2021-09-15 14:36:59 -0700

    allows for one or both params and batch inputs

--- R/cromwellSubmitBatch.R
+++ R/cromwellSubmitBatch.R
@@ -30,8 +30,11 @@ cromwellSubmitBatch <-
     }
     bodyList <- list(
       workflowSource = httr::upload_file(WDL))
-    if(is.null(Params) == F) bodyList <- c(bodyList, workflowInputs = list(httr::upload_file(Params)))
-    if(is.null(Batch) == F) bodyList <- c(bodyList, workflowInputs_2 = list(httr::upload_file(Batch)))
+
+    if(is.null(Params) == F & is.null(Batch) == F) { bodyList <- c(bodyList, workflowInputs = list(httr::upload_file(Params), httr::upload_file(Batch)))
+    } else if (is.null(Params) == F) { bodyList <- c(bodyList, workflowInputs = list(httr::upload_file(Params)))
+    } else if  (is.null(Batch) == F) { bodyList <- c(bodyList, workflowInputs = list(httr::upload_file(Batch))) }
+
     if(is.null(Dependencies) == F) bodyList <- c(bodyList, workflowDependencies = list(httr::upload_file(Dependencies)))
     if(is.null(Options) == F) bodyList <- c(bodyList, workflowOptions = list(httr::upload_file(Options)))
     if(is.null(Labels) == F) bodyList <- c(bodyList, labels = list(jsonlite::toJSON(as.list(Labels), auto_unbox = TRUE)))
