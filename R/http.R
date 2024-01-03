42f776263726f1fd2722275ce6414b1a94c889f1
Scott Chamberlain <sachamber@fredhutch.org>
2024-01-02 16:17:08 -0800

    remove fs pkg not needed

--- R/http.R
+++ R/http.R
@@ -10,7 +10,6 @@ httpPOST <- function(url, as = NULL, ...) {
 	httr::content(result, as = as)
 }
 
-#' @importFrom fs path
 make_url <- function(...) {
 	base_url <- Sys.getenv('CROMWELLURL')
 	file.path(base_url, ...)
