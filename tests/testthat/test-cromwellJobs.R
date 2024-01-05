661d5ff300473122dbaee15df4acb53c0d32b840
Scott Chamberlain <sachamber@fredhutch.org>
2024-01-04 16:49:09 -0800

    dontrun eg for workflowOptions; change to match requests on only method, host and path for jobs b/c date in query

--- tests/testthat/test-cromwellJobs.R
+++ tests/testthat/test-cromwellJobs.R
@@ -1,7 +1,7 @@
 test_that("cromwellJobs", {
   vcr::use_cassette("cromwellJobs", {
     res <- cromwellJobs()
-  })
+  }, match_requests_on = c("method", "host", "path"))
 
   expect_s3_class(res, "tbl")
   expect_equal(NCOL(res), 8)
