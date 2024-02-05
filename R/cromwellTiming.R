70127ce985611eb247c315c96df3824fe4b984b8
Scott Chamberlain <sachamber@fredhutch.org>
2024-02-05 14:22:39 -0800

    cromwell_timing: change to using httr::BROWSE with note, fix param usage from template

--- R/cromwellTiming.R
+++ R/cromwellTiming.R
@@ -2,14 +2,18 @@
 #'
 #' @export
 #' @template workflowid
-#' @template serverdeets
+#' @param url (character) base url for your Cromwell server. optional. if not
+#' supplied set the url as the env var `CROMWELLURL`
 #' @inheritSection workflow_options Important
 #' @author Amy Paguirigan, Scott Chamberlain
+#' @details Internally this function uses [httr::BROWSE()] which uses
+#' [utils::browseURL()] - if a auth header is required you'll have to
+#' do that manually
 #' @return Opens a timing diagram in a browser
 cromwell_timing <- function(workflow_id, url = cw_url()) {
   check_url(url)
   crom_mssg("Getting timing diagram from Cromwell")
-  utils::browseURL(
+  httr::BROWSE(
     make_url(
       url,
       "api/workflows/v1",
