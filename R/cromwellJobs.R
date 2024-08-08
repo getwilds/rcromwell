754d8af48e2d34a222d1ef5daeb3ec99f9d7afd6
Scott Chamberlain <sachamber@fredhutch.org>
2024-08-08 09:19:40 -0700

    styling

--- R/cromwellJobs.R
+++ R/cromwellJobs.R
@@ -106,8 +106,10 @@ cromwell_jobs_process <- function(jobs_data) {
   }
 
   # specific column order if columns exist (via `any_of`)
-  columns_order <- c("workflow_name", "workflow_id", "status", "submission", "start",
-    "end", "workflowDuration")
+  columns_order <- c(
+    "workflow_name", "workflow_id", "status", "submission", "start",
+    "end", "workflowDuration"
+  )
   cr_table <- dplyr::relocate(cr_table, any_of(columns_order))
   return(cr_table)
 }
